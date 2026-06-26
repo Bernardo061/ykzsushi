848d7b0e3e24ab5aa0ccbb0aca1c1af6289d1d32
Bernardo061 <joaobernadobrito@sempreceub.com>
2026-06-26 02:09:48 -0300

    Corrigindo

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // Em produção (GitHub Pages) os arquivos ficam em /<nome-do-repo>/.
-  // Ajuste "ykzsushi" para o nome exato do seu repositório no GitHub.
-  publicPath: process.env.NODE_ENV === "production" ? "/ykzsushi/" : "/",
+  publicPath: "/"
 });
