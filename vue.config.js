971f51a959523c04a7355bb0b0755c66f89264f2
Bernardo061 <joaobernadobrito@sempreceub.com>
2026-06-26 01:17:05 -0300

    Corrigindo duplicidade do defineConfig

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true
-})
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/ykzsushi/' // <--- ADICIONE ESTA LINHA EXATAMENTE ASSIM
 })
